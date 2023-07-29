8e7543bfaffffb141be3fea9b25ecbf6f051463e
hinata000 <hinata0125125@icloud.com>
2023-07-29 22:46:14 +0900

    headerを追加してsearch機能を有効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :set_search
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_search
     @q = Animation.ransack(params[:q])
     @animations = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(16)
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:name, :anikomi_id, :image])
   end
 end
