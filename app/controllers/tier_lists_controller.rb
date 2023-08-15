05e31ca2d9567fefaac34b445315ebbbc856a3f1
hinata000 <hinata0125125@icloud.com>
2023-08-15 16:07:07 +0900

    fontawesomeを導入

--- app/controllers/tier_lists_controller.rb
+++ app/controllers/tier_lists_controller.rb
@@ -3,13 +3,14 @@ class TierListsController < ApplicationController
   def new
     @tier_list = TierList.new(animation_id: params[:animation_id])
     @animation = Animation.find_by(id: params[:animation_id])
+    session[:previous_url] = request.referer
   end
 
   def create
     @tier_list = TierList.new(tier_list_params)
     @tier_list.user_id = current_user.id
     if @tier_list.save
-      redirect_to animation_path(@tier_list.animation_id), notice: "レビューを作成しました"
+      redirect_to session[:previous_url], notice: "TierListに追加しました"
     else
       @animation = Animation.find_by(id: params[:animation_id])
       render :new, status: :unprocessable_entity
