64113fa72f98b4285f7e2b82fb10a0a11411e87c
hinata000 <hinata0125125@icloud.com>
2023-08-28 17:49:12 +0900

    bookmarkコントローラーを作成してcreateアクションとdestroyアクションを追加

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,14 @@
+class BookmarksController < ApplicationController
+
+  def create
+    @bookmark = Bookmark.new(user_id: current_user.id, animation_id: params[:animation_id])
+    @bookmark.save
+    redirect_to animation_path(params[:animation_id])
+  end
+
+  def destroy
+    @bookmark = Bookmark.find_by(user_id: current_user.id, animation_id: params[:animation_id])
+    @bookmark.destroy
+    redirect_to animation_path(params[:animation_id]) 
+  end
+end
