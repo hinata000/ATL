e08d24947eaa0055b0a6499c2f542de28dfc6c72
hinata000 <hinata0125125@icloud.com>
2023-08-31 19:59:26 +0900

    TierListの原型を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @ss_tier = @user.tier_lists.where(tier_score: 5)
+    @s_tier = @user.tier_lists.where(tier_score: 4)
+    @a_tier = @user.tier_lists.where(tier_score: 3)
+    @b_tier = @user.tier_lists.where(tier_score: 2)
+    @c_tier = @user.tier_lists.where(tier_score: 1)
   end
 end
