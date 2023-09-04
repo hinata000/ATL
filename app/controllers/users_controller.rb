9a9c4183cbcafe61c616433813c0f78f9376c06e
hinata000 <hinata0125125@icloud.com>
2023-09-05 00:51:30 +0900

    ユーザーページにレビュー表示機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @tier_list = @user.tier_lists
+    @tier_list_entier = @user.tier_list_entiers
+    @tier_list_mix = @tier_list | @tier_list_entier
+    @tier_list_mix.sort!{ |a, b| b.created_at <=> a.created_at }
+
     @ss_tier = @user.tier_lists.where(tier_score: 5)
     @s_tier = @user.tier_lists.where(tier_score: 4)
     @a_tier = @user.tier_lists.where(tier_score: 3)
