fdc1e4cf5df915e42e1a5f0c386905dd6a9d2919
hinata000 <hinata0125125@icloud.com>
2023-09-04 21:22:56 +0900

    プロフィール画面のtierlistをパーシャルに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tier_list = @user.tier_lists
     @ss_tier = @user.tier_lists.where(tier_score: 5)
     @s_tier = @user.tier_lists.where(tier_score: 4)
     @a_tier = @user.tier_lists.where(tier_score: 3)
     @b_tier = @user.tier_lists.where(tier_score: 2)
     @c_tier = @user.tier_lists.where(tier_score: 1)
+
+    @ss_tier_entier = @user.tier_list_entiers.where(tier_score: 5)
+    @s_tier_entier = @user.tier_list_entiers.where(tier_score: 4)
+    @a_tier_entier = @user.tier_list_entiers.where(tier_score: 3)
+    @b_tier_entier = @user.tier_list_entiers.where(tier_score: 2)
+    @c_tier_entier = @user.tier_list_entiers.where(tier_score: 1)
   end
 end
