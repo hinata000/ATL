0244629b06b0c39a1ede608a0a43d29efddc5e4c
hinata000 <hinata0125125@icloud.com>
2023-09-06 00:11:52 +0900

    image_uploader.rbをheader_image_uploader.rbに変更しuser_image.rbを作成

--- app/uploaders/header_image_uploader.rb
+++ app/uploaders/header_image_uploader.rb
@@ -1,7 +1,10 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class HeaderImageUploader < CarrierWave::Uploader::Base
+  mount_uploader :header_image, ImageUploader
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  process resize_to_fit: [650, 350]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -35,9 +38,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
