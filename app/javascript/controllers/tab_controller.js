ed212efca8341a69eee14f9884af718e86517ece
hinata000 <hinata0125125@icloud.com>
2023-09-11 21:10:51 +0900

    users/showへタブ機能を追加

--- app/javascript/controllers/tab_controller.js
+++ app/javascript/controllers/tab_controller.js
@@ -2,6 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tab"
 export default class extends Controller {
-  connect() {
+  static targets = ["tab", "content"]
+
+  tabClick(event){
+    const tabs = this.tabTargets
+    const current = event.currentTarget
+    const currentIndex = tabs.indexOf(current)
+    const contents = this.contentTargets
+
+    tabs.forEach((tab, index)=>{
+      if(current.classList.contains("not-active")){
+        tab.classList.remove("is-active")
+        tab.classList.add("not-active")
+        contents[index].classList.add("hidden")
+      }
+    })
+
+    if(current.classList.contains("not-active")){
+      current.classList.remove("not-active")
+      current.classList.add("is-active")
+      contents[currentIndex].classList.remove("hidden")
+    }
   }
 }
