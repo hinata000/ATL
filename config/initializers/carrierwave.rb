84c94b86c3fc3c0f6a151d1fd13dfceff76140c2
hinata000 <hinata0125125@icloud.com>
2023-10-19 00:34:52 +0900

    config.fog_public = falseを追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,14 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_provider = 'fog/aws'
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
-    config.fog_directory = "ENV['S3_BUCKET_NAME']"
+    config.fog_directory = ENV['S3_BUCKET_NAME']
     config.cache_storage = :fog
   else
     config.storage = :file
