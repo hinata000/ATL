a92ded87d119ecc53ba1f8ccb9174fb5ec4d7ff0
hinata000 <hinata0125125@icloud.com>
2023-10-22 03:19:19 +0900

    不要な文字を削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    email { |n| "user@example.com" }
+    email { "user@example.com" }
     user_name { "user" }
-    user_id { |n| "user" }
+    user_id { "user" }
     password { 'password' }
     password_confirmation { 'password' }
   end
