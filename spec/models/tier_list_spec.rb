16c324004e912f8a4d209cd477d39d1d404dc979
hinata000 <hinata0125125@icloud.com>
2023-10-22 04:27:09 +0900

    TierListのテストを作成

--- spec/models/tier_list_spec.rb
+++ spec/models/tier_list_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe TierList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tier_list) { create(:tier_list) }
+
+  context 'user_idがない場合' do
+    before { tier_list.user_id = nil }
+
+    example '作成できないこと' do
+      expect(tier_list).to be_invalid
+    end
+  end
+
+  context 'animation_idがない場合' do
+    before { tier_list.user_id = nil }
+
+    example '作成できないこと' do
+      expect(tier_list).to be_invalid
+    end
+  end
+
+  context 'スコア、spoilerが入力された場合' do
+    example '作成できること' do
+      expect(tier_list).to be_valid
+    end
+  end
+
+  context 'スコアが入力されていない場合' do
+    before { tier_list.tier_score = nil }
+
+    example '作成できないこと' do
+      expect(tier_list).to be_invalid
+    end
+  end
+
+  context 'スコアが1から5以外の数値である場合' do
+    before { tier_list.tier_score = 0 }
+
+    example '作成できないこと' do
+      expect(tier_list).to be_invalid
+    end
+  end
+
+  context 'レビューが501文字以上である場合' do
+    before { tier_list.comment = 'a' * 501 }
+
+    example '作成できないこと' do
+      expect(tier_list).to be_invalid
+    end
+  end
 end
