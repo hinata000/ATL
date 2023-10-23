558781789d2653d2afc44ccdeadc282239c0d3fc
hinata000 <hinata0125125@icloud.com>
2023-10-24 02:42:06 +0900

    authenticate_userからshowを除外

--- app/controllers/tier_lists_controller.rb
+++ app/controllers/tier_lists_controller.rb
@@ -1,5 +1,5 @@
 class TierListsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   def create
     @tier_list = TierList.new(tier_list_params)
