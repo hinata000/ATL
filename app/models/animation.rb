4455cfa8599e6d8578875de505bf43030a6e3957
hinata000 <hinata0125125@icloud.com>
2023-10-27 04:06:52 +0900

    不要なコメントを削除

--- app/models/animation.rb
+++ app/models/animation.rb
@@ -1,10 +1,10 @@
 class Animation < ApplicationRecord
   enum season: { spring: 1, summer: 2, autumn: 3, winter: 4 }
+
   has_one :animation_detail
   has_many :tier_lists, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
 
-  # Annictから情報を取得
   def import_from_annict
     base_url = "https://api.annict.com/v1"
     access_token = ENV["ANNICT_ACCESS_TOKEN"]
@@ -15,21 +15,19 @@ class Animation < ApplicationRecord
 
     (start_year..end_year).each do |year|
       seasons.each.with_index(1) do |season, index|
-        # 初回リクエストはデータの総数を調べるために実行
+
         data = JSON.parse(Faraday.get("#{base_url}/works?fields=id&per_page=50&filter_season=#{year}-#{season}&sort_watchers_count=desc&access_token=#{access_token}").body)
 
-        data_count = data["total_count"]         # データの数
-        page_count = (data_count / 50.to_f).ceil # ページの数
+        data_count = data["total_count"]
+        page_count = (data_count / 50.to_f).ceil
 
         current_page = 1
 
-        # 現在のページ <= ページの数になるまで繰り返し処理を実行
         while current_page <= page_count do
           data = JSON.parse(Faraday.get("#{base_url}/works?fields=title,title_kana,images,twitter_username,official_site_url,media_text,syobocal_tid,season_name_text&page=#{current_page}&per_page=50&filter_season=#{year}-#{season}&sort_watchers_count=desc&access_token=#{access_token}").body)
           animations = data["works"]
 
           animations.each do |animation|
-            # すでにレコードが存在する場合は更新、無ければ新規作成
             Animation.find_or_initialize_by(title: animation["title"]).update(
               title_kana: animation["title_kana"],
               year: year,
